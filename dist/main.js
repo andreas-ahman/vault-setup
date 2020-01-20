a4d3c68f172275e9a4bbf571da478f6c9fe0c5bc
Andreas Åhman <andreas.ahman@ingka.ikea.com>
2020-01-20 10:16:32 +0100

    Revert to manual releases

---
+++ dist/main.js
@@ -0,0 +1,63 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core = __importStar(require("@actions/core"));
+const axios_1 = __importDefault(require("axios"));
+const fs_1 = __importDefault(require("fs"));
+function vaultLogin(baseUrl, headers, roleId, secretId) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const client = axios_1.default.create({
+            headers,
+            baseURL: baseUrl,
+        });
+        const url = '/v1/auth/approle/login';
+        core.debug(`Logging in via ${baseUrl}${url}`);
+        const res = yield client.post(url, { role_id: roleId, secret_id: secretId });
+        core.debug('Token generated successfully');
+        return res.data.auth.client_token;
+    });
+}
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const url = core.getInput('url', { required: true });
+            const roleId = core.getInput('role_id', { required: true });
+            const secretId = core.getInput('secret_id', { required: true });
+            const namespace = core.getInput('namespace', { required: false });
+            core.debug('Loaded variables:');
+            core.debug(`- url: ${url}`);
+            core.debug(`- role_id: ${roleId}`);
+            core.debug(`- secret_id: ${secretId}`);
+            core.debug(`- namespace: ${namespace}`);
+            const headers = {};
+            if (namespace != null) {
+                headers['X-Vault-Namespace'] = namespace;
+            }
+            const vaultToken = yield core.group('Login using approle', () => vaultLogin(url, headers, roleId, secretId));
+            headers['X-Vault-Token'] = vaultToken;
+            fs_1.default.writeFileSync('.vault_token', vaultToken);
+        }
+        catch (error) {
+            core.setFailed(error.message);
+        }
+    });
+}
+run();
